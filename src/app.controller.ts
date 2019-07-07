bbff59f9cc9682b379a68cdb1506e7337319f0ef
Justin Tillman <tillman.55@buckeyemail.osu.edu>
2019-07-07 00:38:20 -0400

    trying different way of pathing

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Get, Res } from '@nestjs/common';
-import * as path from 'path';
+import { join } from 'path';
 import { Response } from 'express';
 
 @Controller()
 export class AppController {
   @Get()
   root(@Res() response: Response) {
-    response.sendFile(path.resolve('./dist/client/index.html'));
+    // response.sendFile(path.resolve('./dist/client/index.html'));
+    // response.sendFile(join(__dirname, '..', 'client', 'index.html'));
   }
 }
--- src/middleware/frontend/frontend.middleware.ts
+++ src/middleware/frontend/frontend.middleware.ts
@@ -1,7 +1,7 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { apiPrefix } from '../../config';
 import { Request, Response } from 'express';
-import * as path from 'path';
+import { join, resolve } from 'path';
 
 const allowedExtentions = [
   '.js',
@@ -17,7 +17,7 @@ const allowedExtentions = [
 
 const prefix = apiPrefix;
 
-const resolvePath = (file: string) => path.resolve(`./dist/client/${file}`);
+// const resolvePath = (file: string) => resolve(`./dist/client/${file}`);
 
 @Injectable()
 export class FrontendMiddleware implements NestMiddleware {
@@ -30,9 +30,11 @@ export class FrontendMiddleware implements NestMiddleware {
       allowedExtentions.filter(extention => baseUrl.indexOf(extention) > 0)
         .length > 0
     ) {
-      res.sendFile(resolvePath(baseUrl));
+      // res.sendFile(resolvePath(baseUrl));
+      res.sendFile(join(__dirname, '..', '..', '..', 'client', baseUrl));
     } else {
-      res.sendFile(resolvePath('index.html'));
+      // res.sendFile(resolvePath('index.html'));
+      res.sendFile(join(__dirname, '..', '..', '..', 'client', 'index.html'));
     }
   }
 }
