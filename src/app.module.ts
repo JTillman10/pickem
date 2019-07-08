bba0d17d1645781f10964a9419f1732c4c784b7c
Justin Tillman <tillman.55@buckeyemail.osu.edu>
2019-07-08 02:15:19 -0400

    removing changes for client

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
-import { Module, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
 import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { getPostgresConnectionOptions } from './config';
 import { GameModule } from './modules/game/game.module';
 import { PicksModule } from './modules/pick/pick.module';
-import { FrontendMiddleware } from './middleware/frontend/frontend.middleware';
 
 @Module({
   imports: [
@@ -17,12 +15,5 @@ import { FrontendMiddleware } from './middleware/frontend/frontend.middleware';
     GameModule,
     PicksModule,
   ],
-  controllers: [AppController],
 })
-export class AppModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(FrontendMiddleware)
-      .forRoutes({ path: '/*', method: RequestMethod.ALL });
-  }
-}
+export class AppModule {}
